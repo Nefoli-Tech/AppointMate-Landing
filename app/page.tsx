2b4fdafe8bf538d657dfdc4ba11b4165c1a6c726
r0shish <roshish152002@gmail.com>
2023-05-10 10:34:48 +0545

    chores: remove unneccessary files and codes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Hero from '~/components/widgets/Hero';
-import SocialProof from '../src/components/widgets/SocialProof';
 import Features3 from '~/components/widgets/Features3';
 import Steps from '~/components/widgets/Steps';
 import Testimonial from '~/components/widgets/Testimonial';
 import FAQs2 from '~/components/widgets/FAQs2';
 import Team from '~/components/widgets/Team';
-import CallToAction2 from '~/components/widgets/CallToAction2';
 import {
-  callToActionData,
   faqsData2,
   featuresData,
   heroData,
